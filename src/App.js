ce7423f812bc929a92ea8470aa3e7f08e0b099f1
seoyuun <carpedm1221@gmail.com>
2022-01-06 12:42:21 +0900

    components 폴더 생성, 각 메시지 객체 생성

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+import "./App.css";
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="3a906d46-4257-41ac-bed8-3747be740a33"
+      userName="Seoyum"
+      userSecret="1234"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}></ChatFeed>}
+    />
+  );
+};
+
+export default App;
